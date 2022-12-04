4e251ca11d6b0a4ed5ce753b3d11b4803bf0f34a
Bilal <113285404+Bilal-Abykeev@users.noreply.github.com>
2022-12-04 19:25:06 +0300

    Add files via upload

---
+++ dz6/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import UserPage from "./pages/userPage/UserPage";
+
+function App() {
+  return (
+    <div className="App">
+        <UserPage/>
+    </div>
+  );
+}
+
+export default App;
